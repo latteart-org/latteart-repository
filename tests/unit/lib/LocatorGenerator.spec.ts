03b4c7658ba031a39ed8127c344731a63738e008
Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>
2023-01-30 17:04:10 +0900

    fixed not to use HTML encode in locators
    Signed-off-by: Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>

--- tests/unit/lib/LocatorGenerator.spec.ts
+++ tests/unit/lib/LocatorGenerator.spec.ts
@@ -64,6 +64,43 @@ describe("LocatorGeneratorImpl", () => {
         const locator = generator.generateFrom(element);
         expect(locator).toBe("xpath1");
       });
+      it("textにHTML特殊文字が含まれている場合はxpathを返す", () => {
+        const element1: ElementLocatorSource = {
+          tagname: "button",
+          text: "<xxx",
+          xpath: "xpath1",
+          attributes: { value: "value1" },
+        };
+        const locator1 = generator.generateFrom(element1);
+        expect(locator1).toBe("xpath1");
+
+        const element2: ElementLocatorSource = {
+          tagname: "button",
+          text: ">xxx",
+          xpath: "xpath2",
+          attributes: { value: "value2" },
+        };
+        const locator2 = generator.generateFrom(element2);
+        expect(locator2).toBe("xpath2");
+
+        const element3: ElementLocatorSource = {
+          tagname: "button",
+          text: "/xxx",
+          xpath: "xpath3",
+          attributes: { value: "value3" },
+        };
+        const locator3 = generator.generateFrom(element3);
+        expect(locator3).toBe("xpath3");
+
+        const element4: ElementLocatorSource = {
+          tagname: "button",
+          text: "x xx",
+          xpath: "xpath4",
+          attributes: { value: "value4" },
+        };
+        const locator4 = generator.generateFrom(element4);
+        expect(locator4).toBe("xpath4");
+      });
     });
   });
 });
