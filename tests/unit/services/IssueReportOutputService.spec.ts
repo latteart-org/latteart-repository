433dac8b3a8148a5f33d8932a9265834dd5792bb
Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>
2023-02-03 15:31:34 +0900

    modify issue report output test
    Signed-off-by: Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>

--- tests/unit/services/IssueReportOutputService.spec.ts
+++ tests/unit/services/IssueReportOutputService.spec.ts
@@ -36,7 +36,7 @@ describe("IssueReportOutputService", () => {
       memo: "memo",
     };
 
-    it("渡したデータからFindings、TestPurposesのシートに分けてExcelが出力される", async () => {
+    it("渡したデータが目的ありだった場合Findings、TestPurposesのシートに分けてExcelが出力される", async () => {
       const service = new IssueReportOutputServiceImpl();
 
       const row1 = {
@@ -131,6 +131,93 @@ describe("IssueReportOutputService", () => {
       ]);
     });
 
+    it("渡したデータが目的なしだった場合、Findings、TestPurposesのシートに分けてExcelが出力される", async () => {
+      const service = new IssueReportOutputServiceImpl();
+
+      const row1 = {
+        ...commonRow,
+        testPurposeValue: "",
+        testPurposeDetails: "",
+        noteValue: "noteValue1",
+        noteDetails: "noteDetails1",
+        tags: "bug,test",
+      };
+
+      const row2 = {
+        ...commonRow,
+        testPurposeValue: "",
+        testPurposeDetails: "",
+        noteValue: "noteValue2",
+        noteDetails: "noteDetails2",
+        tags: "",
+      };
+
+      const reportSource = {
+        testMatrixName: "testMatrixName",
+        rows: [row1, row2],
+      };
+
+      service.output(tmpDirPath, reportSource);
+
+      const filePath = path.join(
+        tmpDirPath,
+        `${reportSource.testMatrixName}.xlsx`
+      );
+
+      const workbook = XLSX.readFile(filePath);
+      const sheet1 = workbook.Sheets["Findings"];
+      const findings = XLSX.utils.sheet_to_json(sheet1);
+
+      const sheetNames = workbook.SheetNames;
+
+      expect(sheetNames.length).toEqual(2);
+      expect(sheetNames[0]).toEqual("Findings");
+      expect(sheetNames[1]).toEqual("TestPurposes");
+
+      expect(findings).toEqual([
+        {
+          GroupName: "groupName",
+          TestTargetName: "testTargetName",
+          ViewPointName: "viewPointName",
+          Session: "sessionName",
+          Tester: "tester",
+          Memo: "memo",
+          TestPurpose: "",
+          TestPurposeDetail: "",
+          Finding: "noteValue1",
+          FindingDetail: "noteDetails1",
+          Tags: "bug,test",
+        },
+        {
+          GroupName: "groupName",
+          TestTargetName: "testTargetName",
+          ViewPointName: "viewPointName",
+          Session: "sessionName",
+          Tester: "tester",
+          Memo: "memo",
+          TestPurpose: "",
+          TestPurposeDetail: "",
+          Finding: "noteValue2",
+          FindingDetail: "noteDetails2",
+          Tags: "",
+        },
+      ]);
+
+      const sheet2 = workbook.Sheets["TestPurposes"];
+      const testPurposes = XLSX.utils.sheet_to_json(sheet2);
+
+      expect(testPurposes).toEqual([
+        {
+          GroupName: "groupName",
+          TestTargetName: "testTargetName",
+          ViewPointName: "viewPointName",
+          Session: "sessionName",
+          TestPurpose: "",
+          TestPurposeDetail: "",
+        },
+      ]);
+    });
+
     it("渡したデータのセッション内に目的が重複するデータがある場合はTestPurposesのシート内で重複削除された結果が出力される", async () => {
       const service = new IssueReportOutputServiceImpl();
 
@@ -212,6 +299,102 @@ describe("IssueReportOutputService", () => {
           TestTargetName: "testTargetName",
           ViewPointName: "viewPointName",
           Session: "sessionName",
+          TestPurpose: "testPurposeValue1",
+          TestPurposeDetail: "testPurposeDetails1",
+        },
+      ]);
+    });
+
+    it("渡したデータが複数セッションだった場合、Findings、TestPurposesのシートに分けてExcelが出力される", async () => {
+      const service = new IssueReportOutputServiceImpl();
+
+      const row1 = {
+        ...commonRow,
+        testPurposeValue: "testPurposeValue1",
+        testPurposeDetails: "testPurposeDetails1",
+        noteValue: "noteValue1",
+        noteDetails: "noteDetails1",
+        tags: "bug,test",
+      };
+
+      const row2 = {
+        ...commonRow,
+        sessionName: "sessionName2",
+        testPurposeValue: "testPurposeValue1",
+        testPurposeDetails: "testPurposeDetails1",
+        noteValue: "noteValue1",
+        noteDetails: "noteDetails1",
+        tags: "",
+      };
+
+      const reportSource = {
+        testMatrixName: "testMatrixName",
+        rows: [row1, row2],
+      };
+
+      service.output(tmpDirPath, reportSource);
+
+      const filePath = path.join(
+        tmpDirPath,
+        `${reportSource.testMatrixName}.xlsx`
+      );
+
+      const workbook = XLSX.readFile(filePath);
+      const sheet1 = workbook.Sheets["Findings"];
+      const findings = XLSX.utils.sheet_to_json(sheet1);
+
+      const sheetNames = workbook.SheetNames;
+
+      expect(sheetNames.length).toEqual(2);
+      expect(sheetNames[0]).toEqual("Findings");
+      expect(sheetNames[1]).toEqual("TestPurposes");
+
+      expect(findings).toEqual([
+        {
+          GroupName: "groupName",
+          TestTargetName: "testTargetName",
+          ViewPointName: "viewPointName",
+          Session: "sessionName",
+          Tester: "tester",
+          Memo: "memo",
+          TestPurpose: "testPurposeValue1",
+          TestPurposeDetail: "testPurposeDetails1",
+          Finding: "noteValue1",
+          FindingDetail: "noteDetails1",
+          Tags: "bug,test",
+        },
+        {
+          GroupName: "groupName",
+          TestTargetName: "testTargetName",
+          ViewPointName: "viewPointName",
+          Session: "sessionName2",
+          Tester: "tester",
+          Memo: "memo",
+          TestPurpose: "testPurposeValue1",
+          TestPurposeDetail: "testPurposeDetails1",
+          Finding: "noteValue1",
+          FindingDetail: "noteDetails1",
+          Tags: "",
+        },
+      ]);
+
+      const sheet2 = workbook.Sheets["TestPurposes"];
+      const testPurposes = XLSX.utils.sheet_to_json(sheet2);
+
+      expect(testPurposes).toEqual([
+        {
+          GroupName: "groupName",
+          TestTargetName: "testTargetName",
+          ViewPointName: "viewPointName",
+          Session: "sessionName",
+          TestPurpose: "testPurposeValue1",
+          TestPurposeDetail: "testPurposeDetails1",
+        },
+        {
+          GroupName: "groupName",
+          TestTargetName: "testTargetName",
+          ViewPointName: "viewPointName",
+          Session: "sessionName2",
           TestPurpose: "testPurposeValue1",
           TestPurposeDetail: "testPurposeDetails1",
         },
